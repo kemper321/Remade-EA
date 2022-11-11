9397d9630d2913dd0d32e1bde169b829cc2189c6
kemper321 <117703410+kemper321@users.noreply.github.com>
2022-11-11 11:15:30 -0500

    Create Thoughts-Ideas.lua

---
+++ Thoughts-Ideas.lua
@@ -0,0 +1,19 @@
+--
+--make a dropdown that changes value for a variable for teleports
+--Leechy yt inf spins update for gui
+
+--example
+--dropdown
+blablabla, function(area)
+  Tp = area
+ end
+
+
+
+if variable = forge then
+  game.Player.LocalPlayer.HumanoidRootpart.CFrame = CFrame.new(forge)
+end,
+
+if variable = Lavender Respite
+  game.Player.LocalPlayer.HumanoidRootPart.CFrame = CFrame.new(Lavender Respite)
+ end,
